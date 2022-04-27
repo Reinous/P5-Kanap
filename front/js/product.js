67c7fe6d4c6afabfd057b82e1c9fdf6b1c99470c
Kevin <reinous@gmail.com>
2022-04-27 10:06:04 +0200

    Page produit fini a 97% Local storage marche

--- front/js/product.js
+++ front/js/product.js
@@ -51,14 +51,57 @@ document.querySelector('#addToCart').addEventListener('click',(event) => {
     let color = document.getElementById("colors").value;
     let quantity = document.getElementById("quantity").value;
 
-    let product = [{
-        produit: id,
+    let sofa = {
+        products: id,
         quantities: quantity,
         colors: color,
-    }]
-    //Mise de l'object dans le local storage
-localStorage.setItem('cart' ,JSON.stringify(product))
-const cart = localStorage.getItem('cart')
-   console.log(JSON.parse(cart))
+    }
+
+    //Verifier si une couleur et une quantité est noté
+    if(color && quantity > 0 && quantity <= 100){
+    
+        //Mise de l'object dans le local storage
+        addSofa(sofa);
+    }
+    //Sinon erreur
+    else if(color == false || quantity <= 0 || quantity > 100){
+        alert("Veuillez de remplir tous les champs correctement");
+    }
+
 })
+
+//Creation du local storage
+function saveCart(sofa){
+    localStorage.setItem('cart' ,JSON.stringify(sofa))
+}
+
+//Verification si le panier est vide sinon le remettre en tableau
+function getSofa(){
+    let sofa = localStorage.getItem('cart');
+    if(sofa == null){
+        return [];
+    }else{
+        return JSON.parse(sofa);
+    }
+}
+
+//Voir si le canapé existe deja dans le panier ou pas en verifiant la couleur et id du produit sinn push
+function addSofa(product){
+    let sofa = getSofa();
+    let foundProduct = sofa.find(p => p.products == product.products && p.colors == product.colors);
+
+    //si il existe on les additionne
+    if(foundProduct != undefined){
+        let numberA = parseInt(foundProduct.quantities, 10);
+        let numberB = parseInt(quantity.value, 10);
+        foundProduct.quantities = numberA += numberB;
+    }else{
+        //sinon on le push a la fin de la liste
+        product.quantities = parseInt(quantity.value, 10);
+        sofa.push(product);
+    }
+    saveCart(sofa);
+    const essay = localStorage.getItem('cart')
+console.log(JSON.parse(essay))
+}
 
