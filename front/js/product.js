a74e841503084ea27533518a07d6d2b255c15219
Kevin <reinous@gmail.com>
2022-05-02 10:33:30 +0200

    Script et produit fini normalement

--- front/js/product.js
+++ front/js/product.js
@@ -1,107 +1,106 @@
 // URL de API
 const urlApi = 'http://localhost:3000/api/products/';
 
-function addColorSelection(product){
-    const selectColor = document.getElementById('colors');
-    for(couleur of product.colors){
-        let option = document.createElement('option');
-        option.setAttribute('product', couleur);
-        option.textContent = couleur;
-        selectColor.appendChild(option);
-    }
+function addColorSelection(product) {
+	const selectColor = document.getElementById('colors');
+	for (couleur of product.colors) {
+		let option = document.createElement('option');
+		option.setAttribute('product', couleur);
+		option.textContent = couleur;
+		selectColor.appendChild(option);
+	}
 }
 
 //url searchparams pour récupérer l'id et le mettre avec l'api
 const params = new URL(document.location).searchParams;
 const id = params.get('id');
-const urlApiProduits = new URL (id, urlApi)
+const urlApiProduits = new URL(id, urlApi);
 
 //mettre en liens les canapé et les id
 fetch(urlApiProduits)
-    .then(function(response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then(function(product){
-        //mettre en relations les info produit avec le html
+	.then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+	})
+	.then(function (product) {
+		//mettre en relations les info produit avec le html
 
+		//a changer innerhtml
+		document.querySelector(
+			'.item__img'
+		).innerHTML = `<img src="${product.imageUrl}" alt="Photographie d'un canapé"></img>`;
+		document.querySelector('#title').textContent = product.name;
+		document.querySelector('#price').textContent = product.price;
+		document.querySelector('#description').textContent = product.description;
 
-        //a changer innerhtml
-        document.querySelector(".item__img").innerHTML = `<img src="${product.imageUrl}" alt="Photographie d'un canapé"></img>`;
-        document.querySelector("#title").textContent = product.name ;
-        document.querySelector('#price').textContent = product.price ; 
-        document.querySelector('#description').textContent = product.description ;
+		//Pour la selection de couleur
+		addColorSelection(product);
+	})
+	.catch(function (err) {
+		alert("Une erreur s'est produite" + err);
+	});
 
-
-        //Pour la selection de couleur
-        addColorSelection(product)
-    })
-    .catch(function(err) {
-        alert("Une erreur s'est produite")
-    })
-
-
-    //**********************Local Storage***************************** */
+//**********************Local Storage***************************** */
 
 //Ecouter le bouton du panier
 
-document.querySelector('#addToCart').addEventListener('click',(event) => {
-    //Creation de l'objet avant de mettre dans le local storage
-    let color = document.getElementById("colors").value;
-    let quantity = document.getElementById("quantity").value;
+document.querySelector('#addToCart').addEventListener('click', (event) => {
+	//Creation de l'objet avant de mettre dans le local storage
+	let color = document.getElementById('colors').value;
+	let quantity = document.getElementById('quantity').value;
 
-    let sofa = {
-        products: id,
-        quantities: quantity,
-        colors: color,
-    }
+	let sofa = {
+		id: id,
+		quantities: quantity,
+		colors: color,
+	};
 
-    //Verifier si une couleur et une quantité est noté
-    if(color && quantity > 0 && quantity <= 100){
-    
-        //Mise de l'object dans le local storage
-        addSofa(sofa);
-    }
-    //Sinon erreur
-    else if(color == false || quantity <= 0 || quantity > 100){
-        alert("Veuillez de remplir tous les champs correctement");
-    }
-
-})
+	//Verifier si une couleur et une quantité est noté
+	if (color && quantity > 0 && quantity <= 100) {
+		//Mise de l'object dans le local storage
+		addSofa(sofa);
+		alert('Votre article a bien été ajouté au panier.');
+	}
+	//Sinon erreur
+	else if (color == false || quantity <= 0 || quantity > 100) {
+		alert('Veuillez selectionner une couleur et une quantité entre 0 et 100.');
+	}
+});
 
 //Creation du local storage
-function saveCart(sofa){
-    localStorage.setItem('cart' ,JSON.stringify(sofa))
+function saveCart(sofa) {
+	localStorage.setItem('cart', JSON.stringify(sofa));
 }
 
 //Verification si le panier est vide sinon le remettre en tableau
-function getSofa(){
-    let sofa = localStorage.getItem('cart');
-    if(sofa == null){
-        return [];
-    }else{
-        return JSON.parse(sofa);
-    }
+function getSofa() {
+	const sofa = localStorage.getItem('cart');
+	if (sofa === null) {
+		return [];
+	} else {
+		return JSON.parse(sofa);
+	}
 }
 
 //Voir si le canapé existe deja dans le panier ou pas en verifiant la couleur et id du produit sinn push
-function addSofa(product){
-    let sofa = getSofa();
-    let foundProduct = sofa.find(p => p.products == product.products && p.colors == product.colors);
+function addSofa(product) {
+	const sofa = getSofa();
+	const foundProduct = sofa.find(
+		(p) => p.id == product.id && p.colors == product.colors
+	);
 
-    //si il existe on les additionne
-    if(foundProduct != undefined){
-        let numberA = parseInt(foundProduct.quantities, 10);
-        let numberB = parseInt(quantity.value, 10);
-        foundProduct.quantities = numberA += numberB;
-    }else{
-        //sinon on le push a la fin de la liste
-        product.quantities = parseInt(quantity.value, 10);
-        sofa.push(product);
-    }
-    saveCart(sofa);
-    const essay = localStorage.getItem('cart')
-console.log(JSON.parse(essay))
+	//si il existe on les additionne
+	if (foundProduct !== undefined) {
+		const numberA = parseInt(foundProduct.quantities, 10);
+		const numberB = parseInt(quantity.value, 10);
+		foundProduct.quantities = numberA + numberB;
+	} else {
+		//sinon on le push a la fin de la liste
+		product.quantities = parseInt(quantity.value, 10);
+		sofa.push(product);
+	}
+	saveCart(sofa);
+	const essay = localStorage.getItem('cart');
+	console.log(JSON.parse(essay));
 }
-
--- front/js/script.js
+++ front/js/script.js
@@ -1,92 +1,59 @@
-
-// fetch("http://localhost:3000/api/products")
-//     .then(function(response){
-//         if (response.ok){
-//             return response.json().then((products) => {console.log(products);
-//             const affichage = document.createElement('ul');
-//             affichage.classList.add('myClass')
-//             for (let product of products){
-//                 const item = document.createElement('li')
-//                 item.textContent = `${product.name} ${product.description}`;
-//                 affichage.appendChild(item)
-//             }
-//             console.log(affichage)
-//             const items = document.getElementById("items");
-//             items.appendChild(affichage)
-//         });
-//     }
-// })
-
-
 // URL de API
 
 const urlApi = 'http://localhost:3000/api/products';
 
-
 //*****************Local storage exemple */
-const voitur =[
-    {id:'renault',
-        quantities: 5,
-        colors: 'rouge',},
-    {id:'BM',
-        quantities: 2,
-        colors: 'Vert',}
-]
+// const voitur =[
+//     {id:'renault',
+//         quantities: 5,
+//         colors: 'rouge',},
+//     {id:'BM',
+//         quantities: 2,
+//         colors: 'Vert',}
+// ]
 
-//mise en chaine de caractere grace a "stringify"
-localStorage.setItem('test' ,JSON.stringify(voitur))
-const item = localStorage.getItem('test')
-console.log(item)
-// remise en tableau de la constante grace a "parse"
-console.log(JSON.parse(item))
+// //mise en chaine de caractere grace a "stringify"
+// localStorage.setItem('test' ,JSON.stringify(voitur))
+// const item = localStorage.getItem('test')
+// console.log(item)
+// // remise en tableau de la constante grace a "parse"
+// console.log(JSON.parse(item))
 
+// const essay = localStorage.getItem('cart')
+// console.log(JSON.parse(essay))
 
 fetch(urlApi)
-    .then(function(response) {
-        if (response.ok) {
-            return response.json();
-        }
-    })
-    .then(function(products) {
-        const item = document.getElementById("items");
-        for(const article of products){
+	.then(function (response) {
+		if (response.ok) {
+			return response.json();
+		}
+	})
+	.then(function (products) {
+		const item = document.getElementById('items');
+		for (const article of products) {
+			const A = document.createElement('a');
+			A.href = `./product.html?id=${article._id}`;
+			item.appendChild(A);
 
-            const A = document.createElement('a')
-            A.href = `./product.html?id=${article._id}`
-            item.appendChild(A)
-            
+			const articles = document.createElement('article');
+			A.appendChild(articles);
 
-            const articles = document.createElement('article')
-            A.appendChild(articles)
+			const img = new Image();
+			img.src = `${article.imageUrl}`;
+			img.alt = `${article.altTxt}`;
+			articles.appendChild(img);
 
-            const img = new Image
-            img.src = `${article.imageUrl}`
-            img.alt = `${article.altTxt}`
-            articles.appendChild(img)
+			const title = document.createElement('h3');
+			title.classList.add('productNam');
+			title.textContent = `${article.name}`;
+			articles.appendChild(title);
 
-            const title = document.createElement('h3')
-            title.classList.add('productNam')
-            title.textContent = `${article.name}`
-            articles.appendChild(title)
-
-            const productDescription = document.createElement('p')
-            productDescription.classList.add('productDescription')
-            productDescription.textContent = `${article.description}`
-            articles.appendChild(productDescription)
-
-
-            // document.getElementById('items').innerHTML += `<a href="./product.html?id=${articles._id}">
-            // <articles>
-            // <img src="${articles.imageUrl}" alt="${articles.altTxt}">
-            // <h3 class="productName">${articles.name}</h3>
-            // <p class="productDescription">${articles.description}</p>
-            // </articles>
-            // </a>` 
-        }
-
-        // item.appendChild(items)
-
-    }) 
-    .catch(function(err) {
-        console.log(err);
-    })
+			const productDescription = document.createElement('p');
+			productDescription.classList.add('productDescription');
+			productDescription.textContent = `${article.description}`;
+			articles.appendChild(productDescription);
+		}
+	})
+	.catch(function (err) {
+		console.log(err);
+	});
